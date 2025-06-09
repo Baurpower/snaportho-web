651ea547bd872982ee03d52c176ff32c28cc7534
Baurpower <alexbaur123@gmail.com>
2025-06-08 22:39:06 -0400

    idk

--- src/app/learn/update-password/page.tsx
+++ src/app/learn/update-password/page.tsx
@@ -1,7 +1,8 @@
 // src/app/learn/update-password/page.tsx
 
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
+
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
