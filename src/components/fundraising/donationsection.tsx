6c396a21535a3096fd72a9aa27e771920f984224
Baurpower <alexbaur123@gmail.com>
2025-06-24 15:49:50 -0400

    Vs Studio being weird

--- src/components/fundraising/donationsection.tsx
+++ src/components/fundraising/donationsection.tsx
@@ -2,7 +2,7 @@
 
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import DonationForm from './DonationForm';
+import DonationForm from './donationform';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
